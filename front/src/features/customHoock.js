72b734d8ac9ca2f3d4b4416f56ce0cadd84a1c63
shichchirtch <ivanvol9l@gmail.com>
2026-02-16 23:30:50 +0100

    34 COMMIT
    debug

--- front/src/features/customHoock.js
+++ front/src/features/customHoock.js
@@ -5,8 +5,10 @@ export function useTranslation() {
     const lan = useSelector(state => state.user.language)
 
     function t(key) {
-        return translations[lan]?.[key] || key
+        const key_for_dict = translations[lan]?.[key] || key
+        console.log('key_for_dict = ', key_for_dict)
+        return key_for_dict
     }
-    console.log('t = ', t)
+
     return { t }
 }

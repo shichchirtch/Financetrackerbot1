f769f08f2834d23ff07146d697bf2be3cda7c884
shichchirtch <ivanvol9l@gmail.com>
2026-02-20 00:19:07 +0100

    54 COMMIT
    После увеличения объёма оперативнки

--- front/src/pages/MonthsBoard.jsx
+++ front/src/pages/MonthsBoard.jsx
@@ -1,27 +1,44 @@
 import MonthsGrid from "./MonthsGrid";
-import ButtonBack from "../components/common/ButtonBack.jsx";
+// import ButtonBack from "../components/common/ButtonBack.jsx";
 import { useNavigate } from 'react-router-dom'
 
-
 export default function MonthsBoard() {
-    console.log("MONTHBOARD 19.02")
     const navigate = useNavigate()
-    console.log("typeof navigate =", typeof navigate)
+
+    function goToBalance(month) {
+        console.log("NAV FUNC TYPE =", typeof navigate)
+        navigate(`/balance/${month}`)
+    }
 
     return (
-        <div>
-            <h2 className="text-center text-2xl mt-8 font-bold text-gray-100">
-                2026
-            </h2>
-
-            <MonthsGrid
-                onSelect={(month) => navigate(`/balance/${month}`)}
-            />
-            <div className="flex justify-center mt-6">
-                <ButtonBack />
-            </div>
-        </div>
+        <MonthsGrid onSelect={goToBalance} />
     )
 }
+
+
+
+
+
+
+// export default function MonthsBoard() {
+//     console.log("MONTHBOARD 19.02-1")
+//     const navigate = useNavigate()
+//     console.log("typeof navigate =", typeof navigate)
+//
+//     return (
+//         <div>
+//             <h2 className="text-center text-2xl mt-8 font-bold text-gray-100">
+//                 2026
+//             </h2>
+//
+//             <MonthsGrid
+//                 onSelect={(month) => navigate(`/balance/${month}`)}
+//             />
+//             <div className="flex justify-center mt-6">
+//                 <ButtonBack />
+//             </div>
+//         </div>
+//     )
+// }
 
 

6d0cdda743acc5642033e5d33255e96a5622aa85
shichchirtch <ivanvol9l@gmail.com>
2026-02-20 01:15:40 +0100

    59 COMMIT
    debug

--- front/src/components/layout/Layout.jsx
+++ front/src/components/layout/Layout.jsx
@@ -4,8 +4,8 @@ import Footer from "./Footer";
 import { useTranslation } from "../../features/customHoock";
 
 function Layout() {
-    const wa =  window.Telegram?.WebApp?.initDataUnsafe
-    const tgUser = wa?.initDataUnsafe?.user?.first_name
+    const wa =  '2'//window.Telegram?.WebApp?.initDataUnsafe
+    const tgUser = '1' //wa?.initDataUnsafe?.user?.first_name
 
     const { t } = useTranslation()
 

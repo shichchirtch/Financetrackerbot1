40a6b9325db51c90c248f4ebe9a17f5b34ad8836
shichchirtch <ivanvol9l@gmail.com>
2026-02-20 01:21:12 +0100

    60 COMMIT
    debug

--- front/src/pages/MonthsBoard.jsx
+++ front/src/pages/MonthsBoard.jsx
@@ -2,51 +2,56 @@ import MonthsGrid from "./MonthsGrid";
 import ButtonBack from "../components/common/ButtonBack.jsx";
 import { useNavigate } from 'react-router-dom'
 
-export default function MonthsBoard() {
-    const navigate = useNavigate();
-
-    function goToBalance(month) {
-        console.log("NAV FUNC TYPE =", typeof navigate);
-        navigate(`/balance/${month}`);
-    }
-
-    return (
-        <div className="min-h-screen flex flex-col items-center pt-10 px-4">
-
-            <h2 className="text-3xl font-bold text-gray-100 mb-8">
-                2026
-            </h2>
-
-            <div className="w-full max-w-md">
-                <MonthsGrid onSelect={goToBalance} />
-            </div>
-
-            <div className="mt-10">
-                <ButtonBack />
-            </div>
-
-        </div>
-    );
-}
-
-
-
-
-
-
 // export default function MonthsBoard() {
-//     const navigate = useNavigate()
+//     const navigate = useNavigate();
 //
 //     function goToBalance(month) {
-//         console.log("NAV FUNC TYPE =", typeof navigate)
-//         navigate(`/balance/${month}`)
+//         console.log("NAV FUNC TYPE =", typeof navigate);
+//         navigate(`/balance/${month}`);
 //     }
 //
 //     return (
-//         <MonthsGrid onSelect={goToBalance} />
+//         <div className="min-h-screen flex flex-col items-center pt-10 px-4">
 //
-//     )
+//             <h2 className="text-3xl font-bold text-gray-100 mb-8">
+//                 2026
+//             </h2>
+//
+//             <div className="w-full max-w-md">
+//                 <MonthsGrid onSelect={goToBalance} />
+//             </div>
+//
+//             <div className="mt-10">
+//                 <ButtonBack />
+//             </div>
+//
+//         </div>
+//     );
 // }
+
+
+
+
+
+
+export default function MonthsBoard() {
+    const navigate = useNavigate()
+
+    function goToBalance(month) {
+        console.log("NAV FUNC TYPE =", typeof navigate)
+        navigate(`/balance/${month}`)
+    }
+
+    return (<div>
+        <div>
+        <MonthsGrid onSelect={goToBalance} />
+            </div>
+        <div className="mt-10">
+                <ButtonBack />         </div>
+        </div>
+
+    )
+}
 
 
 

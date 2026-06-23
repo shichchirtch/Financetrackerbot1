5270ffac6de9bc3508aef72137e0a9d4ce2e65ac
shichchirtch <ivanvol9l@gmail.com>
2026-06-24 00:30:29 +0200

    37 - 63  COMMIT Загрузка видео инструкции

--- back/command_handlers.py
+++ back/command_handlers.py
@@ -1,10 +1,10 @@
-from aiogram import Router
+from aiogram import Router, F
 from filters import IS_ADMIN
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
 from bot_instance import ROOT_WIND, ADMIN, ABOUT
-from aiogram_dialog import  DialogManager, StartMode
+from aiogram_dialog import  DialogManager, StartMode, ShowMode
 from my_fast_api import redis_db
 from lexicon import *
 from user_repo import *
@@ -12,6 +12,10 @@ from user_repo import *
 
 ch_router = Router()
 
+@ch_router.message(F.video)
+async def video_id_geber_messages(message: Message):
+    data = message.video.file_id
+    print('\n\nvidos tocken\n\n',data)
 
 @ch_router.message(CommandStart())
 async def command_start_process(message: Message,dialog_manager: DialogManager, state: FSMContext
@@ -40,7 +44,15 @@ async def command_help(message: Message, dialog_manager: DialogManager):
 
 @ch_router.message(Command('basic_menu'))
 async def basic_menu_start(message: Message, dialog_manager: DialogManager):
-    await message.answer('basic menu')
+    # await message.answer('basic menu')
+    await dialog_manager.reset_stack()
+    await dialog_manager.start(state=ROOT_WIND.do_nothing)
+
+@ch_router.message(Command('presentation'))
+async def provide_presentation(message: Message,  dialog_manager: DialogManager):
+    video_presentation = 'BAACAgIAAxkBAAINBWdv5xZKBAjtV3p-ThlpdMpMxxsIAAJXXQACyNSAS8TQD540NahaNgQ'
+
+    await message.answer_video(video=video_presentation, caption="🖥 Video Instruction")
     await dialog_manager.reset_stack()
     await dialog_manager.start(state=ROOT_WIND.do_nothing)
 
